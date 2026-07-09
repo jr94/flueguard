eab606a0f27a197f1760a0b718253acebb562c80
jr94 <jose.riquelme94@gmail.com>
2026-07-09 11:21:43 -0400

    fix

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,12 @@
 import { Controller, Get, Res } from '@nestjs/common';
-import { AppService } from './app.service';
 import type { Response } from 'express';
 import { join } from 'path';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return 'Hello World!';
   }
 
   @Get('links')
